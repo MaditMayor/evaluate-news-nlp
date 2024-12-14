e4f6c591167f5e96fa94de6c15b11df4ff4e67e7
Madit Mayor <maditm23@gmail.com>
2024-12-14 14:41:18 +0200

    Update webpack.prod.js

--- starter_project/webpack.prod.js
+++ starter_project/webpack.prod.js
@@ -1,33 +1,48 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-        }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new WorkboxPlugin.GenerateSW()
+  entry: "./src/client/index.js",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  mode: "production",
+  optimization: {
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
-    devServer: {
-        port: 3000,
-        allowedHosts: 'all'
-    }
-}
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new WorkboxPlugin.GenerateSW(),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
+  devServer: {
+    port: 3000,
+    allowedHosts: "all",
+  },
+};
