f0f24136601fa2bc21c127d47fb9a8757a063002
Madit Mayor <maditm23@gmail.com>
2024-12-14 14:51:54 +0200

    Update index.js

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,9 +1,16 @@
 // js files
-import { handleSubmit } from './js/formHandler'
+import { handleSubmit, sendData } from "./js/formHandler";
+import { urlCheck } from "./js/urlChecker";
 
-
-
-// alert("I EXIST")
+// alert("I EXIST");
 // console.log("CHANGE!!");
 
 // sass files
+import "./styles/main.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+import "./styles/resets.scss";
+
+export { handleSubmit, sendData, urlCheck };
